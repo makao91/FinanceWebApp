f876f8c19fb295e65c92a9a7d62e62d11e36021d
Marcin Kapela <marcin.kapela.programista@gmail.com>
2020-04-29 11:26:19 +0200

    Edit password work correctly

--- edit-pass.php
+++ edit-pass.php
@@ -5,7 +5,7 @@
 $user_pass = $_SESSION['password'];
 $old_implemented_pass = $_POST['passOldEdit'];
 $new_pass = $_POST['passEdit'];
-$new_pass_hash = password_hash($_POST['passEdit'], PASSWORD_DEFAULT);
+$new_pass_hash = password_hash($new_pass, PASSWORD_DEFAULT);
 $user_id = $_SESSION['id'];
 
 mysqli_report(MYSQLI_REPORT_STRICT);
@@ -17,29 +17,30 @@ try
   	throw new Exception(mysqli_connect_errno());
   }
 
-  if(password_verify($new_pass, $_SESSION['password']))
+  if(password_verify($new_pass, $_SESSION['password'])==false)
     {
-      echo 1;
+      if(password_verify($old_implemented_pass, $_SESSION['password']))
+      {
+          $sqlUpdatePass = "
+          UPDATE users SET password = '$new_pass_hash'
+          WHERE id = '$user_id'";
+          if($connect->query($sqlUpdatePass))
+          {
+            $_SESSION['password'] = $new_pass_hash;
+            echo 1;
+          }
+          else
+          {
+            throw new Exception($connect->error);
+          }
+     	}
+      else {
+        echo 2; //your implemented old pass is wrong
+      }
     }
-  else if(password_verify($old_implemented_pass, $_SESSION['password']))
-    {
-        $sqlUpdatePass = "
-        UPDATE users SET password = '$new_pass_hash'
-        WHERE id = '$user_id'";
-        if($connect->query($sqlUpdatePass))
-        {
-          $_SESSION['password'] = $new_pass_hash;
-          echo true;
-        }
-        else
-        {
-          throw new Exception($connect->error);
-        }
-   	}
-  else
-    {
-      echo false; //eneven old and implemented old pass
-    }  
+    else {
+      echo 3;  //old and new pass cannot be the same
+    }
 }
 catch (Exception $e)
 {
